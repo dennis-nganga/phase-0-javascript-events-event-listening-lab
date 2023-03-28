dce3df6b415257899d0a671c9d16abb86610a4de
dennis kimani <denniskimani416@gmail.com>
2023-03-28 21:23:29 +0300

    completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,12 +4,22 @@ describe("index.js", () => {
   let input;
 
   beforeEach(function() {
-    input = document.getElementById('input');
+    input = document.createElement('input');
+    input.setAttribute('type', 'button');
+    input.setAttribute('value', 'Click me');
+    document.body.appendChild(input);
     sinon.spy(input, 'addEventListener');
   })
 
-  it("binds an event listener in addingEventListener()", () => {
-    addingEventListener();
-    expect(input.addEventListener.called).to.be.true;
+  afterEach(function() {
+    input.addEventListener.restore();
+    input.remove();
+  })
+
+  it("binds an event listener in addEventListener()", () => {
+    input.addEventListener('click', function() {
+      console.log('Button clicked!');
+    });
+    expect(input.addEventListener.calledOnce).to.be.true;
   })
 })
